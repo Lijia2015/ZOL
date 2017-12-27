12dcbec84d841b2981da108ac5d6b6ed90edbc0d
Lijia2015 <lijiazhuanyong2126.com>
2017-12-27 18:31:01 +0800

    haha

---
+++ zol/src/components/main/AppMain.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="app-main">
+		这是我的团队开发项目，zol商城开动了
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'app-main'
+	}
+</script>
+
+<style>
+</style>
--- zol/src/router/index.js
+++ zol/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import AppMain from '../components/main/AppMain'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+	{path:'',redirect:'/main'},
+	{path:'/main',name:'main',component:AppMain},
+	{path:'**',redirect:'/main'},
+]
+
+const router = new Router({
+	routes,
 })
+
+
+export default router
