b5f7dccbb80cdcfeac5fc082364352f049c2e6c7
Lijia2015 <lijiazhuanyong2126.com>
2017-12-27 20:59:02 +0800

    bug修复

--- zol/src/components/main/AppMain.vue
+++ zol/src/components/main/AppMain.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="app-main">
-		
+		呵呵呵，很多很多黄金时代颠三倒四出具的市场价四渡赤水
 	</div>
 </template>
 
@@ -11,6 +11,11 @@
 </script>
 
 <style lang="scss">
-	
+	$base-color:red;
+	$font-color:green;
+	.app-main{
+		background: $base-color;
+		color:$font-color;
+	}
 	
 </style>
