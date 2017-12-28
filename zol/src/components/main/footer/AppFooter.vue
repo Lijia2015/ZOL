104d7308e4ef3e79c57934eba1cc8d4baaeff834
Lijia2015 <lijiazhuanyong2126.com>
2017-12-28 21:00:21 +0800

    导航的实现

--- zol/src/components/main/footer/AppFooter.vue
+++ zol/src/components/main/footer/AppFooter.vue
@@ -1,35 +1,51 @@
 <template>
 	<div class="app-footer">
-		<div :key='nav.id' class="foot-nav" v-for="nav in navs" @click="changeNav(nav.path)">
-			<i></i>
-			<span>{{nav.name}}</span>
+		<div class="foot-nav" @click="changeNav('home')" :class="[showType=='home'?'foot-cur':'']">
+			<i class="yo-ico">&#xe640;</i>
+			<span>首页</span>
+		</div>
+		<div class="foot-nav" @click="changeNav('category')" :class="[showType=='category'?'foot-cur':'']">
+			<i class="yo-ico">&#xe6b2;</i>
+			<span>分类</span>
+		</div>
+		<div class="foot-nav" @click="changeNav('car')" :class="[showType=='car'?'foot-cur':'']">
+			<i class="yo-ico">&#xe621;</i>
+			<span>购物车</span>
+		</div>
+		<div class="foot-nav" @click="changeNav('mine')" :class="[showType=='mine'?'foot-cur':'']">
+			<i class="yo-ico">&#xe68f;</i>
+			<span>我的</span>
 		</div>
 	</div>
 </template>
 
 <script>
+	
 	export default{
 		name:'app-footer',
 		data(){
 			return {
-				
+				showType:'home',
 				navs:[
-					{id:1,name:'首页',path:'home'},
-					{id:2,name:'分类',path:'category'},
-					{id:3,name:'购物车',path:'car'},
-					{id:4,name:'我的',path:'mine'},
+					{id:1,name:'首页',path:'home',icon:'&#xe640;'},
+					{id:2,name:'分类',path:'category',icon:'&#xe640;'},
+					{id:3,name:'购物车',path:'car',icon:'&#xe640;'},
+					{id:4,name:'我的',path:'mine',icon:'&#xe640;'},
 				],
 			};
 		},
 		methods:{
 			
-			changeNav(name){
-				
-				this.$router.push(name);
+			changeNav(path){
+
+				this.$router.push(path);
+				this.showType = path;
 			}
 			
 		},
 	}
+	
+
 </script>
 
 <style lang="scss">
