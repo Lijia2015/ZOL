c7c4b2a8c3a618928208dfc29ae3c2a6d1b9b83b
Lijia2015 <lijiazhuanyong2126.com>
2018-01-05 21:37:13 +0800

    hh

--- zol/src/components/main/content/children/home/AppBanner.vue
+++ zol/src/components/main/content/children/home/AppBanner.vue
@@ -6,6 +6,7 @@
 	        <div class="swiper-slide"><img src="http://i3.mercrt.fd.zol-img.com.cn/g5/M00/08/0C/ChMkJlpAX2WIfiNCAAIHYHjj0mQAAjdvQJmDBQAAgd4253.jpg"/></div>
 	        <div class="swiper-slide"><img src="http://i2.mercrt.fd.zol-img.com.cn/g5/M00/02/0E/ChMkJ1ovvsWIUxD6AAJROjQ_kxoAAjGSANBOvEAAlFS768.jpg"/></div>
 	    </div>
+
 	    <!-- 如果需要分页器 -->
 	    <div class="swiper-pagination"></div>
 	    
