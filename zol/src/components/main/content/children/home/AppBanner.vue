89ff0e96ce0c6cbd9b66e2a34d8e6d4438538966
yinxingen <yinxingen@163.com>
2018-01-05 20:54:01 +0800

    home

---
+++ zol/src/components/main/content/children/home/AppBanner.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="app-banner swiper-container">
+		
+	    <div class="swiper-wrapper banner">
+	    	<div class="swiper-slide"><img src="http://i4.mercrt.fd.zol-img.com.cn/g5/M00/07/07/ChMkJ1o8bp6IHLmvAAKnHhxEBlgAAjYfwMiHLkAAqc2104.jpg"/></div>
+	        <div class="swiper-slide"><img src="http://i3.mercrt.fd.zol-img.com.cn/g5/M00/08/0C/ChMkJlpAX2WIfiNCAAIHYHjj0mQAAjdvQJmDBQAAgd4253.jpg"/></div>
+	        <div class="swiper-slide"><img src="http://i2.mercrt.fd.zol-img.com.cn/g5/M00/02/0E/ChMkJ1ovvsWIUxD6AAJROjQ_kxoAAjGSANBOvEAAlFS768.jpg"/></div>
+	    </div>
+	    <!-- 如果需要分页器 -->
+	    <div class="swiper-pagination"></div>
+	    
+	</div>
+</template>
+<script>
+	import Swiper from 'swiper' 
+	export default {
+		name:"app-banner",
+		
+		mounted(){
+	    	new Swiper('.app-banner',{
+				loop:true,
+				autoplay: {
+					disableOnInteraction: false,
+				},
+				pagination:{
+				    el: '.swiper-pagination',
+				    clickable :true,
+				},
+			})
+	    }
+	}
+</script>
+
+<style lang="scss">
+	.swiper-pagination-bullet{
+		opacity: 1;
+		background:#fff;
+	}
+	.swiper-pagination-bullet-active {
+		background:red;
+	}
+	.banner{
+		img{
+			height: 1.75rem;
+		}
+	}
+	
+</style>
---
+++ zol/src/components/main/content/children/home/AppNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app-nav">
+     nav
+  </div>
+</template>
+
+<script>
+	import axios from 'axios'
+	export default {
+	    name:"app-nav",
+	    data:function(){
+        return {
+        }
+    },
+	    methods:{
+        getData(){
+            let that = this
+//          http://m.zol.com/index.php?c=Shop_IndexV2&a=AjaxGetGoods&pos=1&page=2
+            axios.get('http://localhost:8080/zol/index.php',{
+                params:{
+                	c:'Shop_IndexV2',
+									a:'AjaxGetGoods',
+									pos:2,
+									page:2
+                }
+            }).then((response)=>{
+                console.log(response)
+            })
+        },
+    },
+    created(){
+        this.getData()
+    },
+	}
+</script>
+<style>
+	
+</style>
