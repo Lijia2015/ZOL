3d0ff1d9b8547d4e8223c97d3096146a9fdaa0cf
Lijia2015 <lijiazhuanyong2126.com>
2018-01-05 22:35:07 +0800

    首页导航

--- zol/src/components/main/content/children/home/AppHome.vue
+++ zol/src/components/main/content/children/home/AppHome.vue
@@ -2,7 +2,6 @@
   <div class="app-home">
      <app-header></app-header>
      <app-banner></app-banner>
-     <app-section></app-section>
      <app-nav></app-nav>
    	<router-view></router-view>
   </div>
@@ -11,12 +10,11 @@
 <script>
 	import AppHeader from './AppHeader.vue'
 	import AppBanner from './AppBanner.vue'
-	import AppSection from './AppSection.vue'
 	import AppNav from './AppNav.vue'
 	export default {
 	    name:"app-home",
 	    components:{
-	    	AppHeader,AppBanner,AppSection,AppNav
+	    	AppHeader,AppBanner,AppNav
 	    }
 	    
 	}
